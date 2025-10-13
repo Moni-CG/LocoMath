db91110b06a421b2aa657e463c01bd449ea1771e
MONIC <MONIC@Monica.home.lla.com>
2025-10-13 15:18:58 -0600

    probando

---
+++ src/Presentacion/PruebaLocoMath.java
@@ -0,0 +1,11 @@
+package Presentacion;
+
+/**
+ *
+ * @author MONIC
+ */
+public class PruebaLocoMath {
+    public static void main(String[] args) {
+        System.out.println("Hola Fio, probando");
+    }
+}
