2596c5d0043894efa1d8c7d8350d98a4bf125b5a
MONIC <MONIC@Monica.home.lla.com>
2025-10-13 14:57:07 -0600

    Proyecto LocoMath

---
+++ src/Entidad/Jugador.java
@@ -0,0 +1,29 @@
+package Entidad;
+
+/**
+ *
+ * @author MONIC
+ */
+public class Jugador {
+    private int idJugador;
+    private String usuario;
+    private int puntaje;
+    private int aciertos;
+    private int errores;
+    private int tiempoRestante;
+
+    public Jugador() {
+    }
+
+    public Jugador(int idJugador, String usuario) {
+        this.idJugador = idJugador;
+        this.usuario = usuario;
+        this.puntaje = puntaje;
+        this.aciertos = aciertos;
+        this.errores = errores;
+        this.tiempoRestante = tiempoRestante;
+    }
+    
+    
+    
+}
